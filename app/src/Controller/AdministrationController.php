893eb92cf272f7280daa536b77f374b881724dc1
Julian Dreier <jupadr@gmail.com>
2022-12-28 21:01:03 +0100

    feat: Benutzer bearbeiten hinzugefügt
    User können nun in der Administration auch bearbeitet werden

--- app/src/Controller/AdministrationController.php
+++ app/src/Controller/AdministrationController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\Persistence\ManagerRegistry;
 use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,6 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdministrationController extends AbstractController
 {
+    
     #[Route('/administration', name: 'app_administration')]
     public function index(): Response
     {
@@ -24,84 +26,165 @@ class AdministrationController extends AbstractController
             'controller_name' => 'AdministrationController',
         ]);
     }
-
+    
     #[Route('/administration/users', name: 'administration_users')]
     public function listUsers(ManagerRegistry $doctrine): Response
     {
         $users = $doctrine->getRepository(User::class)->findAll();
-
+        
         return $this->render('administration/users.html.twig', [
             'users' => $users,
         ]);
     }
-
+    
     #[Route('administration/delete_user/{userId}', name: 'administration_deleteUser')]
     public function deleteUser(int $userId): Response
     {
         return new Response(content: $userId, status: 501);
     }
-
+    
     #[Route('administration/edit_user/{userId}', name: 'administration_editUser')]
-    public function editUser(int $userId): Response
-    {
-        return new Response(content: $userId, status: 501);
-    }
-
-    #[Route('administration/new_user', name: 'administration_newUser')]
-    public function newUser(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
-    {
-        $user = new User();
-
+    public function editUser(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        ManagerRegistry $doctrine,
+        EntityManagerInterface $em,
+        int $userId
+    ): Response {
+        $user = $doctrine->getRepository(User::class)->findOneBy(
+            ['id' => $userId]
+        );
+        
+        if ($user === null) {
+            return new Response(content: 'User not found', status: 404);
+        }
+        
+        $user->eraseCredentials();
+        
         $form = $this->createFormBuilder($user)
             ->add('username', TextType::class, [
-                'label' => 'Benutzname',
+                'label'    => 'Benutzername',
                 'required' => true,
             ])
             ->add('firstname', TextType::class, [
-                'label' => 'Vorname',
+                'label'    => 'Vorname',
                 'required' => true,
             ])
             ->add('lastname', TextType::class, [
-                'label' => 'Nachname',
-                'required' => true,
-                ])
-            ->add('instructor', CheckboxType::class, [
-                'label' => 'Ausbilder',
-                'required' => true,
-                ])
-            ->add('password', TextType::class, [
-                'label' => 'Passwort',
+                'label'    => 'Nachname',
                 'required' => true,
             ])
-            ->add('password_repeat', TextType::class, [
-                'label' => 'Passwort wiederholen',
+            ->add('instructor', CheckboxType::class, [
+                'label'    => 'Ausbilder',
                 'required' => true,
-                'mapped' => false,
+            ])
+            ->add('password', PasswordType::class, [
+                'label'      => 'Neues Passwort',
+                'required'   => false,
+                'empty_data' => '',
+            ])
+            ->add('password_repeat', PasswordType::class, [
+                'label'      => 'Passwort wiederholen',
+                'required'   => false,
+                'mapped'     => false,
+                'empty_data' => '',
             ])
             ->add('save', SubmitType::class, [
-                'label' => 'Benutzer erstellen'
+                'label' => 'Benutzer bearbeiten',
             ])
             ->getForm();
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
-            $user = $form->getData();
+            $newUser = $form->getData();
+            $newUser->setPassword($user->getPassword());
+            $passwordRepeat = $request->get('form') ['password_repeat'];
+            if ($newUser->getPassword() !== ''
+                && $newUser->getPassword() === $passwordRepeat
+            ) {
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $newUser,
+                    $newUser->getPassword()
+                );
+                $newUser->setPassword($hashedPassword);
+            }
+            $em->persist($newUser);
+            $em->flush();
+            
+            return $this->redirectToRoute('administration_users');
+        }
+        
+        $formView = $form->createView();
+        return $this->render('administration/changeUser.html.twig', [
+            'form' => $formView,
+        ]);
+    }
+    
+    #[Route('administration/new_user', name: 'administration_newUser')]
+    public function newUser(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): Response {
+        $user = new User();
+        
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class, [
+                'label'    => 'Benutzername',
+                'required' => true,
+            ])
+            ->add('firstname', TextType::class, [
+                'label'    => 'Vorname',
+                'required' => true,
+            ])
+            ->add('lastname', TextType::class, [
+                'label'    => 'Nachname',
+                'required' => true,
+            ])
+            ->add('instructor', CheckboxType::class, [
+                'label'    => 'Ausbilder',
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'label'    => 'Passwort',
+                'required' => true,
+            ])
+            ->add('password_repeat', PasswordType::class, [
+                'label'    => 'Passwort wiederholen',
+                'required' => true,
+                'mapped'   => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Benutzer erstellen',
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user           = $form->getData();
             $passwordRepeat = $request->get('form')['password_repeat'];
             if ($user->getPassword() !== $passwordRepeat) {
-                throw new RuntimeException("Passwörter müssen übereinstimmen: ".$user->getPassword()." != $passwordRepeat");
+                throw new RuntimeException(
+                    "Passwörter müssen übereinstimmen: " . $user->getPassword()
+                    . " != $passwordRepeat"
+                );
             }
-            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
             $user->setPassword($hashedPassword);
             
             $em->persist($user);
             $em->flush();
-
+            
             return $this->redirectToRoute('administration_users');
         }
-
+        
         $formView = $form->createView();
         return $this->render('administration/newUser.html.twig', [
             'form' => $formView,
         ]);
     }
+    
 }
--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -10,49 +10,50 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
-
+    private ?int    $id       = null;
+    
     #[ORM\Column(length: 180, unique: true)]
     private ?string $username = null;
-
+    
     #[ORM\Column]
-    private array $roles = [];
-
+    private array   $roles    = [];
+    
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
-    private ?string $password = null;
-
+    private ?string $password   = null;
+    
     #[ORM\Column(length: 255)]
-    private ?string $firstname = null;
-
+    private ?string $firstname  = null;
+    
     #[ORM\Column(length: 255)]
-    private ?string $lastname = null;
-
+    private ?string $lastname   = null;
+    
     #[ORM\Column]
-    private ?bool $instructor = null;
-
+    private ?bool   $instructor = null;
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getUsername(): ?string
     {
         return $this->username;
     }
-
+    
     public function setUsername(string $username): self
     {
         $this->username = $username;
-
+        
         return $this;
     }
-
+    
     /**
      * A visual identifier that represents this user.
      *
@@ -60,9 +61,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->username;
+        return (string)$this->username;
     }
-
+    
     /**
      * @see UserInterface
      */
@@ -71,17 +72,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-
+        
         return array_unique($roles);
     }
-
+    
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
-
+        
         return $this;
     }
-
+    
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -89,14 +90,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
-
+    
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
+        
         return $this;
     }
-
+    
     /**
      * @see UserInterface
      */
@@ -104,41 +105,43 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+        $this->password = '';
     }
-
+    
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
-
+    
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
-
+        
         return $this;
     }
-
+    
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
-
+    
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
-
+        
         return $this;
     }
-
+    
     public function isInstructor(): ?bool
     {
         return $this->instructor;
     }
-
+    
     public function setInstructor(bool $instructor): self
     {
         $this->instructor = $instructor;
-
+        
         return $this;
     }
+    
 }
