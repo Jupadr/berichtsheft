bc8b4bc85eb2d97a092a7d20ff9cf8af384bf35f
Julian Dreier <jupadr@gmail.com>
2022-12-29 15:57:48 +0100

    fix:

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -3,29 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Asset;
 
-class RegistrationController extends AbstractController {
-
-    #[Route('/createUser', name:'app_createUser')]
-    public function createUser() 
+class RegistrationController extends AbstractController
+{
+    
+    #[Route('/createUser', name: 'app_createUser')]
+    public function createUser()
     {
-        $lastName = $_POST["lastName"];
+        $lastName  = $_POST["lastName"];
         $firstName = $_POST["firstName"];
-        $mail = $_POST["e-mail"];
+        $mail      = $_POST["e-mail"];
         $birthDate = $_POST["birth-date"];
-        $password = $_POST["passowrd"];
-
+        $password  = $_POST["passowrd"];
     }
-
+    
     #[Route('/registration', name: 'app_registration')]
-    public function renderRegistration() 
+    public function renderRegistration()
     {
         return $this->render('registration-login/registration.html.twig', [
             'controller_name' => 'RegistrationController',
         ]);
     }
-
+    
 }
