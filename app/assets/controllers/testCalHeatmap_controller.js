617af66d01971ace79c5baa364a3af92b96b6683
Julian Dreier <jupadr@gmail.com>
2023-02-08 19:38:02 +0100

    at least a little bit of progress

--- app/assets/controllers/testCalHeatmap_controller.js
+++ app/assets/controllers/testCalHeatmap_controller.js
@@ -1,17 +1,62 @@
-import CalHeatmap from "cal-heatmap"
-import { Tooltip } from 'cal-heatmap'
+import CalHeatmap from '../cal-heatmap.min'
+import Tooltip from '../Tooltip.min'
 
-const cal = new CalHeatmap();
+let cal = new CalHeatmap()
+
 cal.paint({
   date: {
-    start: new Date('2020-09-01'),
+    start: new Date('2020-01-01'),
     min: new Date('2020-09-01'),
-    max: new Date('2023-08-31')
-
+    max: new Date('2021-08-31')
   },
+  range: 1,
   domain: {
-    type: 'month'
+    type: 'year',
+    dynamicDimension: true,
+    label: {
+      position: 'bottom',
+      textAlign: 'start',
+      text: (timestamp) => {
+        return dayjs(timestamp).$y
+      }
+    },
+    subLabel: {
+      text: () => {
+        return dayjs.weekdays().map(d => d[0].toUpperCase())
+      }
+    }
   },
+  subDomain: {
+    type: 'day',
+    radius: 2,
+  },
+  data: {
+    source: [
+      {date: '2020-08-28', value: 10},
+      {date: '2020-07-20', value: 200},
+    ]
+  },
+  scale: {
+    as: 'color',
+    type: 'linear',
+    scheme: 'PRGn',
+    domain: [0, 40]
+  }
 }, [
-  [ Tooltip, { enabled: true, text: (timestamp, value, dayjsDate) => `${timestamp} ${value} ${dayjsDate.toLocalString()}` } ]
-]);
+  [
+    Tooltip,
+    {
+      enabled: true,
+      text: (timestamp, value, dayjsDate) => `${dayjsDate.toString()}`,
+      placement: 'right',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [-15, 5]
+          }
+        }
+      ]
+    }
+  ]
+])
