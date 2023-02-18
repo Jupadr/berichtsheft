e1ef5e6b4a48d76d5817915a0ad8d4438c1b73bc
Julian Dreier <jupadr@gmail.com>
2023-02-18 10:13:31 +0100

    feat: Test-Implementation Kalender

--- app/assets/controllers/testCalHeatmap_controller.js
+++ app/assets/controllers/testCalHeatmap_controller.js
@@ -1,72 +1,78 @@
 import CalHeatmap from '../cal-heatmap.min'
 import Tooltip from '../Tooltip.min'
 
-let cal = new CalHeatmap()
-
-cal.paint({
-  date: {
-    start: new Date('2020-01-01'),
-    min: new Date('2020-09-01'),
-    max: new Date('2021-08-31')
-  },
-  range: 1,
-  domain: {
-    type: 'year',
-    dynamicDimension: true,
-    label: {
-      position: 'bottom',
-      textAlign: 'start',
-      text: (timestamp) => {
-        return dayjs(timestamp).$y
+window.genCalHeatmap = function () {
+  let cal = new CalHeatmap()
+  let calData = JSON.parse(document.getElementById('cal-data').innerText)
+  
+  cal.on('click', (event, timestamp, value) => {
+    let el = document.getElementById('day-sign')
+    
+    let date = new Date(timestamp)
+    
+    el.innerText = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+    let test = window.confirm('Navigate?')
+    
+    if (test) {
+      window.location.assign('/dashboard')
+    }
+  })
+  
+  cal.paint({
+    date: {
+      start: new Date(calData.startDate),
+      min: new Date(calData.minDate),
+      max: new Date(calData.maxDate),
+    },
+    range: 1,
+    domain: {
+      type: 'year',
+      dynamicDimension: true,
+      label: {
+        position: 'bottom',
+        textAlign: 'start',
+        text: (timestamp) => {
+          return dayjs(timestamp).$y
+        }
+      },
+      subLabel: {
+        text: () => {
+          return dayjs.weekdays().map(d => d[0].toUpperCase())
+        }
       }
     },
-    subLabel: {
-      text: () => {
-        return dayjs.weekdays().map(d => d[0].toUpperCase())
-      }
+    subDomain: {
+      type: 'day',
+      radius: 2,
+    },
+    data: {
+      source: calData.data,
+      x: 'date',
+      y: 'value',
+      groupY: 'sum'
+    },
+    scale: {
+      as: 'color',
+      type: 'linear',
+      scheme: 'Turbo',
+      domain: [0, 30],
     }
-  },
-  subDomain: {
-    type: 'day',
-    radius: 2,
-  },
-  data: {
-    source: [
-      {date: '2020-01-01EST', value: 5},
-      {date: '2020-01-02EST', value: 14},
-      {date: '2020-01-03EST', value: 22},
-      {date: '2020-01-04EST', value: 25},
-      {date: '2020-01-05EST', value: 0},
-      {date: '2020-01-06EST', value: 0},
-      {date: '2020-01-07EST', value: 0},
-      {date: '2020-01-08EST', value: 0},
-      {date: '2020-01-09EST', value: 0},
-    ],
-    x: 'date',
-    y: 'value',
-    groupY: 'sum'
-  },
-  scale: {
-    as: 'color',
-    type: 'linear', //
-    scheme: 'Turbo',
-    domain: [0, 30],
-  }
-}, [
-  [
-    Tooltip,
-    {
-      enabled: true,
-      text: (timestamp, value, dayjsDate) => `${dayjsDate.toString()}, ${value}`,
-      placement: 'right',
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [-15, 5]
+  }, [
+    [
+      Tooltip,
+      {
+        enabled: true,
+        text: (timestamp, value, dayjsDate) => `${dayjsDate.toString()}, ${value}`,
+        placement: 'right',
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [-15, 5]
+            }
           }
-        }
-      ]
-    }
-  ]
-])
+        ]
+      }
+    ]
+  ])
+}
--- app/src/Controller/TestCalHeatmapController.php
+++ app/src/Controller/TestCalHeatmapController.php
@@ -8,11 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestCalHeatmapController extends AbstractController
 {
+    
     #[Route('/test/cal/heatmap', name: 'app_test_cal_heatmap')]
     public function index(): Response
     {
+        $calData = (object)[
+            'startDate' => '2020-01-01',
+            'minDate'   => '2021-09-01',
+            'maxDate'   => '2021-08-31',
+            'data'      => [
+                (object)[
+                    'date'  => '2020-01-01EST',
+                    'value' => 5,
+                ],
+                //      {date: '2020-01-01EST', value: 5},
+                //      {date: '2020-01-02EST', value: 14},
+                //      {date: '2020-01-03EST', value: 22},
+                //      {date: '2020-01-04EST', value: 25},
+                //      {date: '2020-01-05EST', value: 0},
+                //      {date: '2020-01-06EST', value: 0},
+                //      {date: '2020-01-07EST', value: 0},
+                //      {date: '2020-01-08EST', value: 0},
+                //      {date: '2020-01-09EST', value: 0},
+            ],
+        ];
+        
         return $this->render('test_cal_heatmap/index.html.twig', [
             'controller_name' => 'TestCalHeatmapController',
+            'caldata'         => json_encode($calData, JSON_THROW_ON_ERROR),
         ]);
     }
+    
 }
